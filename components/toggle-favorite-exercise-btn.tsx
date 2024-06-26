5f983babe36807e319f8792a7a99ec1089e325b5
Łukasz <lukasz.poczta12@gmail.com>
2024-06-26 11:58:19 +0200

    modify toggle-favorite-exercise-btn

--- components/toggle-favorite-exercise-btn.tsx
+++ components/toggle-favorite-exercise-btn.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React from 'react';
+import React, {  useState } from 'react';
 import { Button } from './ui/button';
 import { useAuth } from '@clerk/nextjs';
 import { toggleExerciseToFavorites } from '@/server/actions/actions';
 import { Heart } from 'lucide-react';
+import { redirect } from 'next/navigation';
 
 import { usePathname } from 'next/navigation';
 
@@ -17,20 +18,30 @@ const ToggleFavoriteExerciseBtn = ({
   isAdded,
 }: ToggleFavoriteExerciseBtnType) => {
   const { userId } = useAuth();
+  if (!userId) {
+    redirect('/');
+  }
   const revalidatePathName = usePathname();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      await toggleExerciseToFavorites(id, userId, revalidatePathName);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
       {userId && (
-        <Button
-          size='sm'
-                    onClick={async () =>
-            await toggleExerciseToFavorites(id, userId, revalidatePathName)
-          }>
+        <Button size='sm' onClick={handleClick} disabled={isLoading}>
           {isAdded ? (
-            <Heart className='text-red-400 stroke-[3px] ' />
+            <Heart className='text-red-400 stroke-[3px]' />
           ) : (
-            <Heart className=' stroke-[3px] ' />
+            <Heart className='stroke-[3px]' />
           )}
         </Button>
       )}
